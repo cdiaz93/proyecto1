e5c987c72424db9b271301c1fb138cefb99a777e
Carlos David <cdiazz@sagazsas.com.co>
2016-08-22 09:14:23 -0500

    cambiamos a espaniol pag principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,12 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Bienvenidos</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Su aplicacion esta cargada correctamente. <br>
+                    Esta es la paguina principal de su app, aqui podra
+                    gestionar informacion publica.
                 </div>
             </div>
         </div>
